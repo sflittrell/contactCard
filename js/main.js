e2c216613c043515a9ae9d61580db54dbb8febd4
sflittrell <sflittrell@gmail.com>
2021-02-27 12:19:45 -0500

    added hover hover and large screen capability

---
+++ js/main.js
@@ -0,0 +1,8 @@
+  
+function hover(element) {
+    element.setAttribute('src', './Images/MainImageBackground.jpeg');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', './Images/BobRoss-Square.jpeg');
+  }
